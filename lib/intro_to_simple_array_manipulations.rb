d1b7f5f6eea2050ac8740564aa1d839f45db010c
Tej Charfi <charfi@usc.edu>
2020-08-16 16:04:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,27 @@
 def using_push(arr, str)
     arr.push(str)
 end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+  arr.pop()
+end
+
+
+def pop_with_args(arr)
+  arr.pop()
+  arr.pop()
+end
+
+
+def using_shift(arr)
+  arr.shift()
+end
+
+def shift_with_args(arr)
+arr.shift()
+arr.shift()
+end
